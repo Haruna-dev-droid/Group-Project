a4bf767b186e5bbb14c080e70d2254b530e4597d
John Doe <samailah40@gmail.com>
2025-04-14 13:00:06 +0100

    added responsiveness and javascript functions

---
+++ script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+// GET INPUT ELEMENTS
+const billInput = document.getElementById("bill-input-space");
+const peopleInput = document.getElementById("people-input-space");
+const tipInputBtns = document.querySelectorAll(".select-tip-button");
+const customTipPercent = document.getElementById("custom-tip");
+const totalAmount = document.querySelector(".total-amount");
+const totalAmountPerPerson = document.querySelector(".total-amount-person");
+const errorContainer = document.querySelector(".error-container");
+
+// funtion to update results
+function updateResults(tipAmount, totalAmountValue) {
+  totalAmount.textContent = `$${tipAmount.toFixed(2)}`;
+  totalAmountPerPerson.textContent = `$${totalAmountValue.toFixed(2)}`;
+}
+
+// calculate tip as the user enters the percentage
+tipInputBtns.forEach((inputButton) => {
+  inputButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let tipPercentage =
+      parseFloat(inputButton.innerText.replace("%", "")) / 100;
+    let amount = parseFloat(billInput.value);
+    let people = Number(peopleInput.value);
+
+    if (people <= 0) {
+      document.querySelector(".error").classList.toggle("active");
+      peopleInput.classList.toggle("error-active");
+      return;
+    }
+
+    const tipAmount = (amount * tipPercentage) / people;
+    const totalAmountValue = amount / people + tipAmount;
+
+    updateResults(tipAmount, totalAmountValue);
+  });
+});
+
+// calculate tip as the user enters the custom percentage
+customTipPercent.addEventListener("input", () => {
+  let amount = parseFloat(billInput.value);
+  let people = Number(peopleInput.value);
+  let customTipPercentage = parseFloat(customTipPercent.value) / 100;
+
+  if (
+    !isNaN(amount) &&
+    !isNaN(people) &&
+    people > 0 &&
+    !isNaN(customTipPercentage)
+  ) {
+    const customTip = (amount * customTipPercentage) / people;
+    const totalAmountValue = amount / people + customTip;
+
+    updateResults(customTip, totalAmountValue);
+  } else {
+    // Optional: reset display if inputs are invalid
+    updateResults(0, 0);
+  }
+});
+// clear results
+document.querySelector(".reset").addEventListener("click", () => {
+  totalAmount.innerHTML = "$0.00";
+  totalAmountPerPerson.innerHTML = "$0.00";
+  billInput.value = "";
+  peopleInput.value = "";
+  customTipPercent.value = "";
+  errorContainer.textContent = "";
+});
